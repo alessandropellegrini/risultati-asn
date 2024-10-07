2fabf3237cb96ea19a278cf48acbde9e75614e9f
Alessandro Pellegrini <alessandropellegrini@users.noreply.github.com>
2024-10-07 15:20:19 +0200

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -87,6 +87,6 @@ foreach($settori as $settore) {
 echo "\n$usciti_nuovi nuovi settori pubblicati.\n";
 echo "Usciti $usciti settori su " . count($settori) . ".\n";
 $new_found = "Usciti " . $usciti . " settori su " . count($settori) . ".\n\n" . $new_found;
-$new_found = "# Risultati I Quadrimestre ASN 2023\n\n" . $new_found;
+$new_found = "# Risultati II Quadrimestre ASN 2023\n\n" . $new_found;
 $new_found = "![logo](img/logo-2023.png)\n\n" . $new_found;
 file_put_contents("README.md", $new_found . $cache);
